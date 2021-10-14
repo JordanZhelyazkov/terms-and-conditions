64ecdd868bfbcbf3eb354390145d2165c64ba254
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-14 08:34:15 +0300

    fixes

--- src/Document.js
+++ src/Document.js
@@ -6,7 +6,7 @@ export default function Document(props){
     return( 
     <>
       <h1 className="title" value={"Terms and Conditions"}>Terms and Conditions</h1>
-      <div className="content" >{props.content}</div>
+      <div className="content" >{props.txt}</div>
       <button disabled="true">I Agree</button>
       </>
       );
